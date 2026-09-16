00a66bc190b1f51d8c3d7d34696e87f6d87f7f05
mkaletta <>
2026-09-16 18:44:28 +0200

    Improve font size of objective illustrations, remove dead code

--- visualizations/plot_objectives.py
+++ visualizations/plot_objectives.py
@@ -8,7 +8,7 @@ def plot_objectives_1d(objs):
     assert objs[0].n_dim == 1
     lin = np.linspace(-4, 4)
     x = np.reshape(lin, shape=(1, len(lin)))
-    plt.rcParams.update({'font.size': 14})
+    plt.rcParams.update({'font.size': 16})
     fig, axes = plt.subplots(1, len(objs), figsize=(4 * len(objs), 4))
     axes[0].set_ylabel(r'$F(\mathbf{w})$')
     for ax, obj in zip(axes, objs):
@@ -20,20 +20,17 @@ def plot_objectives_1d(objs):
     plt.savefig('objectives_1d.pdf', dpi=600, transparent=True)
 
 
-def plot_objectives_2d(objs, name='', plot_3d=True):
+def plot_objectives_2d(objs, name=''):
     assert objs[0].n_dim == 2
     lin = np.linspace(-4, 4, 200)
     x1_grid, x2_grid = np.meshgrid(lin, lin)
     x = np.stack([x1_grid.ravel(), x2_grid.ravel()], axis=0)  # shape (2, 200*200)
-    plt.rcParams.update({'font.size': 15})
+    plt.rcParams.update({'font.size': 17})
     fig, axes = plt.subplots(1, len(objs), figsize=(5 * len(objs), 4), subplot_kw={"projection": "3d"})
     for ax, obj in zip(axes, objs):
         y = obj.value_vec(x).reshape(x1_grid.shape)
-        if plot_3d:
-            plot = ax.plot_surface(x1_grid, x2_grid, y, cmap="viridis")
-        else:
-            plot = ax.contourf(x1_grid, x2_grid, y, levels=40, cmap="viridis")
-        fig.colorbar(plot, ax=ax, pad=0.14)
+        plot = ax.plot_surface(x1_grid, x2_grid, y, cmap="viridis")
+        fig.colorbar(plot, ax=ax, pad=0.15)
         ax.set_title(str(obj).title(), y=1.03)
         ax.set_xlabel(r'$\mathbf{w}_1$')
         ax.set_ylabel(r'$\mathbf{w}_2$')
